5b1657830ef12c5ec03c2e746c0cac4dc5bd75df
islam larabi <islamlarabi0@gmail.com>
2025-07-01 07:21:13 +0100

    Update API URLs to use the correct IP address for service requests and authentication

--- api/client_service.js
+++ api/client_service.js
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const api_key = 'http://172.20.10.9:3000/api/v1/';
+const api_key = 'http://192.168.169.76:3000/api/v1/';
 
 const Service_client = {
   getServiceRequestsClient: async () => {
